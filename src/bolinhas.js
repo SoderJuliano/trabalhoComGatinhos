2b3d59ed03f398e5c9e8b8400f624eb49f356941
Juliano <julianosoder1989@gmail.com>
2021-08-09 14:56:00 -0300

    feat improving

--- src/bolinhas.js
+++ src/bolinhas.js
@@ -10,25 +10,31 @@ const Bolinhas = (selecionada) =>{
             if(selecionada.selecionada==0){
                 return(
                     <div className="bolinhas-container">
+                        <div className="ball"></div>
                         <div className="bolao"/>
                         <div className="bolinha"/>
                         <div className="bolinha"/>
+                        <div className="ball"></div>
                     </div>
                 )
             }else if(selecionada.selecionada==1){
                     return(
                         <div className="bolinhas-container">
+                            <div className="ball"></div>
                             <div className="bolinha"/>
                             <div className="bolao"/>
                             <div className="bolinha"/>
+                            <div className="ball"></div>
                         </div>
                     )
             }else{
                 return(
                     <div className="bolinhas-container">
+                        <div className="ball"></div>
                         <div className="bolinha"/>
                         <div className="bolinha"/>
                         <div className="bolao"/>
+                        <div className="ball"></div>
                     </div>
                 )
             }
